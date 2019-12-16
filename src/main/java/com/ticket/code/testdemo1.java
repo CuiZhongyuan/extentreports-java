bf9f84ed0f549311bb8242da9445ea82b5df1197
rootczy <365450906@qq.com>
2019-12-16 18:09:26 +0800

    testNGDemo1

---
+++ src/main/java/com/ticket/code/testdemo1.java
@@ -0,0 +1,10 @@
+package com.ticket.code;
+
+import org.testng.annotations.Test;
+
+public class testdemo1 {
+    @Test
+    public void testDemo1(){
+        System.out.println("第一个testNG测试");
+    }
+}
